094780923000acc91ce1fb64586d18687c81a64a
Robin Neumann <robin.neumann@absolventa.de>
2016-03-25 21:02:33 +0100

    Update contribution after delivering message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   has_many :messages
   has_many :stages
+  has_many :contributions
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -6,4 +6,21 @@ RSpec.describe Message do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to validate_presence_of(:user) }
 
+  describe '#contribution' do
+    subject { FactoryGirl.create :message }
+
+    context 'when not created before' do
+      it 'auto-creates it' do
+        expect(subject.contribution).to be_a Contribution
+        expect(subject.contribution).to be_persisted
+        expect(subject.contribution.content).to eql subject.content
+      end
+    end
+
+    context 'when created before' do
+      let!(:contribution) { FactoryGirl.create :contribution, user: subject.user, stage: subject.stage }
+      it { expect(subject.contribution).to eql contribution }
+    end
+
+  end
 end
